1d9a352a068792d74a74d633ddffd97b5e14dc90
MailsonD <mailssondennis@gmail.com>
2020-05-23 20:10:40 -0300

    build: adicionando formik e yup para controle de forms

--- src/pages/Login/Form/index.js
+++ src/pages/Login/Form/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 
+import './style.css';
+
 function Form() {
 	return (
-		<div>
+		<div className='form-login'>
 			<Button>asdasd</Button>
 			<p></p>
 			<Input title='asdasd' />
