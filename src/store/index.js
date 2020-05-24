87f0bd56cbea465e0acfd3eb676adbe2790e2bfc
MailsonD <mailssondennis@gmail.com>
2020-05-23 22:13:54 -0300

    build: adicionando redux-thunk a aplicacao

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './root.reducer';
 
-const store = createStore(rootReducer);
+const store = applyMiddleware(thunk)(createStore)(
+	rootReducer
+);
 
 export default store;
--- src/store/util/create-reducer.js
+++ src/store/util/create-reducer.js
@@ -1,5 +1,6 @@
 export function createReducer(initialState, handlers) {
 	return function reducer(state = initialState, action) {
+		// eslint-disable-next-line no-prototype-builtins
 		if (handlers.hasOwnProperty(action.type)) {
 			return handlers[action.type](state, action);
 		} else {
